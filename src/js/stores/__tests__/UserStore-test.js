e0f9fd1b5a3dc03198c5a670c01607bb5b76dc83
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-02-25 17:02:04 -0500

    Day's work, now we have stores for users and teams

---
+++ src/js/stores/__tests__/UserStore-test.js
@@ -0,0 +1,19 @@
+jest.dontMock('../../constants/UserConstants');
+jest.dontMock('../UserStore');
+jest.dontMock('object-assign');
+
+describe('UserStore', function() {
+
+    var UserStore;
+
+    beforeEach(function() {
+        UserStore = require('../UserStore');
+    });
+
+    it('should call common to get all teams', function(){
+        var common = require('../../utils/common');
+        UserStore.getAll();
+        expect(common.getAllUsers).toBeCalled();
+    });
+
+});
