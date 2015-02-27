0b595ad0358b261abb27c530ddbaf0ec9c25b588
Irina Muchnik <irina.muchnik@cfpb.gov>
2015-02-26 21:59:24 -0500

    Wired up actions, mostly got all Fluxx components figured out more or less

--- src/js/constants/TeamConstants.js
+++ src/js/constants/TeamConstants.js
@@ -12,11 +12,8 @@
 var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
-  TODO_CREATE: null,
-  TODO_COMPLETE: null,
-  TODO_DESTROY: null,
-  TODO_DESTROY_COMPLETED: null,
-  TODO_TOGGLE_COMPLETE_ALL: null,
-  TODO_UNDO_COMPLETE: null,
-  TODO_UPDATE_TEXT: null
+    TEAM_CREATE: null,
+    TEAM_DESTROY: null,
+    TEAM_ADD_USER_START: null,
+    TEAM_ADD_USER_COMPLETE: null
 });
--- src/js/constants/UserConstants.js
+++ src/js/constants/UserConstants.js
@@ -12,9 +12,9 @@
 var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
-  TODO_CREATE: null,
+  TEAM_CREATE: null,
   TODO_COMPLETE: null,
-  TODO_DESTROY: null,
+  TEAM_DESTROY: null,
   TODO_DESTROY_COMPLETED: null,
   TODO_TOGGLE_COMPLETE_ALL: null,
   TODO_UNDO_COMPLETE: null,
